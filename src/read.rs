3ebb94eaf47aaea4c919431c0673293c031f5d38
elias <eliashaider95@gmail.com>
2025-01-29 11:42:12 +0100

    add utc_millis extraction to get_files_of_dir add barebone subproject client-cli (need to change root's toml into workspace toml and shift current /src to /server subproject after merge with uncommited changes)

--- src/read.rs
+++ src/read.rs
@@ -1,4 +1,9 @@
-use std::{fs, io, path::Path};
+use std::{
+    fs::{self, Metadata},
+    io,
+    path::Path,
+    time::UNIX_EPOCH,
+};
 
 use serde::Serialize;
 
@@ -7,6 +12,7 @@ pub struct FileDescription {
     name: String,
     size_in_kb: u32,
     file_type: String,
+    last_updated_utc_millis: u64,
 }
 
 pub fn init_directory(path: &Path) -> io::Result<()> {
@@ -17,33 +23,46 @@ pub fn init_directory(path: &Path) -> io::Result<()> {
     }
 }
 
-pub fn get_files_of_dir(path: &Path) -> io::Result<Vec<FileDescription>> {
+pub fn get_files_of_dir(path: &Path) -> Result<Vec<FileDescription>, String> {
     let mut descriptions = Vec::new();
 
-    for entry_result in fs::read_dir(path)? {
-        let entry = entry_result?;
+    for entry_result in fs::read_dir(path).map_err(|e| e.to_string())? {
+        let entry = entry_result.map_err(|e| e.to_string())?;
         let path = entry.path();
 
         if path.is_file() {
-            let metadata = entry.metadata()?;
+            let metadata = entry.metadata().map_err(|e| e.to_string())?;
             let file_size_in_bytes = metadata.len();
             let size_in_kb = file_size_in_bytes / 1024;
-            let name = path.file_name()
+            let name = path
+                .file_name()
                 .and_then(|os| os.to_str())
                 .unwrap_or("[invalid utf8]")
                 .to_string();
-            let file_type = path.extension()
+            let file_type = path
+                .extension()
                 .and_then(|os| os.to_str())
                 .unwrap_or("")
                 .to_string();
-
+            let last_updated_utc_millis = get_last_updated(metadata)
+                .ok_or("Could not determine last updated".to_string())?;
             descriptions.push(FileDescription {
                 name,
                 size_in_kb: size_in_kb as u32,
                 file_type,
+                last_updated_utc_millis,
             });
         }
     }
 
     Ok(descriptions)
+}
+
+fn get_last_updated(metadata: Metadata) -> Option<u64> {
+    if let Ok(modified) = metadata.modified() {
+        if let Ok(duration) = modified.duration_since(UNIX_EPOCH) {
+            return Some(duration.as_millis() as u64);
+        }
+    }
+    None
 }
