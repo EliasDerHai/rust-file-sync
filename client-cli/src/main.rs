39650d0a66834eca12443427bf9d6a56fc6a0e3c
elias <eliashaider95@gmail.com>
2025-02-06 09:03:37 +0100

    refac client (minor)

--- client-cli/src/main.rs
+++ client-cli/src/main.rs
@@ -1,7 +1,7 @@
 use crate::config::read_config;
 use futures_util::future::join_all;
 use reqwest::multipart::Form;
-use reqwest::{Client, Error, Response};
+use reqwest::Client;
 use shared::file_event::FileEventType;
 use shared::get_files_of_directory::{
     get_all_file_descriptions, get_file_description, FileDescription,
@@ -19,29 +19,24 @@ mod config;
 async fn main() {
     let (tx, mut rx) = mpsc::channel::<Vec<FileDescription>>(100);
     let dir_to_monitor = match read_config() {
-        Ok(path) => path,
         Err(error) => {
-            eprintln!("Critical error: {:?}", error);
-            return;
+            panic!("Critical error: {:?}", error);
         }
+        Ok(path) => path,
     };
 
     println!("Start monitoring changes in '{:?}'", dir_to_monitor);
     spawn(watch_directory(dir_to_monitor.clone(), tx));
 
     let mut last_scan: Option<Vec<FileDescription>> = None; // maybe persist this ? needed if files are deleted between sessions
-    let mut skip: Option<Vec<FileDescription>> = None;
+    let mut last_deleted_files: Vec<FileDescription> = Vec::new();
     let client = Client::new();
 
     while let Some(scanned) = rx.recv().await {
-        println!("Files scanned: {:?}", scanned);
-
         if let Some(last) = last_scan.clone() {
-            let deleted_files = determine_deleted_files(&last, &scanned);
-            println!("Files deleted: {:?}", deleted_files);
-            let futures = deleted_files
-                .clone()
-                .into_iter()
+            last_deleted_files = determine_deleted_files(&last, &scanned);
+            let futures = last_deleted_files
+                .iter()
                 .map(|deleted| {
                     client
                         .post("http://localhost:3000/delete")
@@ -49,31 +44,38 @@ async fn main() {
                         .send()
                 })
                 .collect::<Vec<_>>();
-            let c = futures.len();
-            join_all(futures)
-                .await
-                .iter()
-                .for_each(|r| println!("{:?}", r));
+            let results = join_all(futures).await;
+            let c = last_deleted_files.len();
             if c > 0 {
-                println!("Sent {} delete events to server", c);
+                println!(
+                    "Sent {} delete events to server: [{}]",
+                    c,
+                    last_deleted_files
+                        .iter()
+                        .map(|desc| desc.file_name.clone())
+                        .collect::<Vec<String>>()
+                        .join(", ")
+                );
+                results
+                    .iter()
+                    .for_each(|r| println!("Server replied: {:?}", r));
             }
-            skip = Some(deleted_files);
         }
 
         match send_to_server_and_receive_instructions(&client, &scanned).await {
+            Err(err) => println!("Error - failed to get instructions from server: {:?}", err),
             Ok(instructions) => {
                 println!(
-                    "{} Instructions received {:?} - skip: {:?}",
+                    "{} Instructions received {:?}",
                     instructions.len(),
-                    instructions,
-                    skip
+                    instructions
                 );
                 for instruction in instructions {
-                    if let (SyncInstruction::Download(ref path), Some(ref skipped)) =
-                        (&instruction, &skip)
+                    if let SyncInstruction::Download(ref path) = &instruction
                     {
-                        if skipped.iter().any(|deleted| deleted.relative_path == *path) {
-                            // no need to follow the upload instruction because we now that this file was just deleted (breaking the loop)
+                        if last_deleted_files.iter().any(|deleted| deleted.relative_path == *path) {
+                            // no need to follow the download instruction,
+                            // because we now that this file was just deleted (breaking the loop)
                             continue;
                         }
                     }
@@ -82,7 +84,6 @@ async fn main() {
                         .unwrap();
                 }
             }
-            Err(err) => println!("Error - failed to get instructions from server: {:?}", err),
         }
 
         last_scan = Some(scanned);
@@ -92,11 +93,11 @@ async fn main() {
 async fn watch_directory(dir: PathBuf, tx: Sender<Vec<FileDescription>>) {
     loop {
         match get_all_file_descriptions(dir.as_path()) {
-            Ok(descriptions) => match tx.send(descriptions).await {
-                Ok(()) => println!("Scanned dir successfully"),
-                Err(error) => eprintln!("Error while scanning {}", error),
-            },
             Err(error) => eprintln!("Could not scan dir - {}", error),
+            Ok(descriptions) => match tx.send(descriptions).await {
+                Err(error) => eprintln!("Error while scanning {}", error),
+                Ok(()) => println!("Scanned dir successfully"),
+            },
         }
 
         tokio::time::sleep(std::time::Duration::from_secs(3)).await;
@@ -155,13 +156,13 @@ async fn execute(client: &Client, instruction: SyncInstruction, root: &Path) -> 
         SyncInstruction::Delete(p) => {
             let p = &p.resolve(root);
             match tokio::fs::remove_file(&p).await {
-                Ok(()) => {
-                    println!("Deleted {} successfully", &p.to_string_lossy());
-                    Ok(())
-                }
                 Err(err) => {
                     eprintln!("Could not follow delete instruction - {}", err);
                     Err(err.to_string())
+                },
+                Ok(()) => {
+                    println!("Deleted {} successfully", &p.to_string_lossy());
+                    Ok(())
                 }
             }
         }
@@ -173,8 +174,9 @@ async fn execute(client: &Client, instruction: SyncInstruction, root: &Path) -> 
 /// [`https://docs.rs/notify/latest/notify/index.html`] could also be an option for later, but
 /// I prefer a more native approach for now
 ///
-/// Note: Deletes will reverb once time as server instructed deletes are again sent to the server as delete events.
-/// This is due to the stateless nature of the client. Since it doesn't do harm, it doesn't make sense to introduce state in order to deal with this reverb.
+/// Note: Deletes will reverb one time as server instructed deletes are again sent to the server as delete events.
+/// This is due to the stateless nature of the client.
+/// Since it doesn't do harm, it doesn't make sense to introduce state in order to deal with this reverb.
 fn determine_deleted_files(
     last: &Vec<FileDescription>,
     curr: &Vec<FileDescription>,
